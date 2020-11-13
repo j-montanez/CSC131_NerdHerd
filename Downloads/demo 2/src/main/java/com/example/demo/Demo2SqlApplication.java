bfc32c5a0583b178a574ad620e59dabc3d0031a3
PhucTruong98 <31789447+PhucTruong98@users.noreply.github.com>
2020-11-12 23:10:33 -0800

    hi

---
+++ Downloads/demo 2/src/main/java/com/example/demo/Demo2SqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2SqlApplication.class, args);
+	}
+
+}
