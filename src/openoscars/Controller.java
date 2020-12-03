2db1c2e18fb10cda09a1c946166b50afecf0ebd5
Skyler Rader <rader06@gmail.com>
2020-12-03 04:19:28 -0800

    Refactored Main into components
    I saw that everything was in main, and wasn't using the Model-View-Controller  design pattern.  JavaFX was designed with the MVC in mind and it's a best practice to use it. I changed everything to have the scenes in FXML and use the controller to handle the events.

--- src/openoscars/Controller.java
+++ src/openoscars/Controller.java
@@ -1,4 +1,94 @@
 package openoscars;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+// The Controller is the class that should handle actions and logic.
 public class Controller {
+
+
+    // Initializing the FXML Scenes
+
+    @FXML
+    public BorderPane home;
+
+    @FXML
+    public BorderPane winners;
+
+    @FXML
+    public BorderPane nominate;
+
+    @FXML
+    public BorderPane search;
+
+    @FXML
+    public BorderPane vote;
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+
+
+    //Navigation Button Handling
+
+    @FXML
+    private void navigateHome(ActionEvent event){
+        Pane view = null;
+        try {
+            view = FXMLLoader.load(getClass().getResource("resources/home.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainBorderPane.setCenter(view);
+    }
+
+    @FXML
+    private void navigateWinners(ActionEvent event){
+        Pane view = null;
+        try {
+            view = FXMLLoader.load(getClass().getResource("resources/winners.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainBorderPane.setCenter(view);
+    }
+
+    @FXML
+    private void navigateNominate(ActionEvent event){
+        Pane view = null;
+        try {
+            view = FXMLLoader.load(getClass().getResource("resources/nominate.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainBorderPane.setCenter(view);
+    }
+
+    @FXML
+    private void navigateSearch(ActionEvent event){
+        Pane view = null;
+        try {
+            view = FXMLLoader.load(getClass().getResource("resources/search.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainBorderPane.setCenter(view);
+    }
+
+    @FXML
+    private void navigateVote(ActionEvent event){
+        Pane view = null;
+        try {
+            view = FXMLLoader.load(getClass().getResource("resources/vote.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mainBorderPane.setCenter(view);
+    }
+
 }
