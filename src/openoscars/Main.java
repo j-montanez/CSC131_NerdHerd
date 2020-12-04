85c67c9d9f15862afd5ee7ef8bf0298d544a77a7
Skyler Rader <rader06@gmail.com>
2020-12-03 23:25:36 -0800

    Icon

--- src/openoscars/Main.java
+++ src/openoscars/Main.java
@@ -1,10 +1,12 @@
 package openoscars;
 
 //javafx imports
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +14,10 @@ public class Main extends Application {
     // This Starts the main Stage (Window), Loads FXML and give it some starting properties.
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("resources/mainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/login.fxml"));
+        primaryStage.getIcons().add(new Image("resources/imgs/next.png"));
         primaryStage.setTitle("OpenOscars");
-        primaryStage.setScene(new Scene(root, 1280, 768));
+        primaryStage.setScene(new Scene(root, 426, 300));
         primaryStage.show();
     }
 
