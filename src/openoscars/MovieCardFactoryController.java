52f1736f83cd05af3aa115296b18ad5e7d2a15fa
Skyler Rader <rader06@gmail.com>
2020-12-08 00:00:47 -0800

    generating cards

--- src/openoscars/MovieCardFactoryController.java
+++ src/openoscars/MovieCardFactoryController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -36,49 +35,30 @@ public class MovieCardFactoryController implements Initializable {
     @FXML
     public BorderPane searchPane;
 
-    public static int year=0;
 
-
+    public static int index = 0;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        MovieArray mv = new MovieArray(1957);
+        List<MovieNS> movies = mv.getMovies();
+        List<String> tit = mv.getYup();
         for (int j=0;j<2;j++) {             // rows
             for (int i=0;i<3;i++) {         // columns
-                MovieArray mv = new MovieArray(1957);
-                year++;
-                List<MovieNS> movies = mv.getMovies();
-                List<String> tit = mv.getYup();
-                createCard(j,i,);
-//                for(int k = 0; k<movies.size();k++) {
-//                    System.out.println(k + " " + movies.get(k).getTitle() + " " + movies.get(i).getGenre());
-//                }
+                createCard(j,i,movies.get(index).getTitle(),"category",movies.get(index).getPoster(),movies.get(index).getPlot());
+                index++;
+                System.out.println(index + " " + movies.get(index).getTitle() + " " + movies.get(index).getPoster() + " " + movies.get(index).getPlot());
+
             }
+
 
         }
 
     }
 
 
-        public void createCard(int row, int column, String title, String category, String poster, String plot) {
+        public void createCard(int row, int column, String titleIn, String categoryIn, String posterIn, String plotIn) {
 
-//      Variables
-        String posterLink =;
-        String titleText = new String("Movie Title");
-        String categoryText = new String("Category");
-        String descriptionText = new String("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit dolor in est congue, a placerat eros congue." +
-                "Vestibulum cursus hendrerit aliquet. Sed accumsan magna sed lectus mattis ornare non in ipsum. Donec sodales nisl justo, at dapibus quam aliquam vi" +
-                "Donec sollicitudin in nisi fringilla suscipit. Duis eu enim interdum, varius nunc eu, ultrices turpis. Nullam turpis mi, ornare nec semper et, scel" +
-                "eget ligula. Nullam non ex consectetur, malesuada metus vel, fringilla orci. Quisque est enim, molestie quis congue ac, vestibulum b.&#10;&#10;Duis" +
-                " elementum nisi lorem, sed ullamcorper dolor dignissim quis. Pellentesque lobortis est ut hendrerit placerat. Etiam porttitor tellus vel pharetra  " +
-                "Mauris sodales odio suscipit ornare ullamcorper. Mauris porta nisi augue, eu sagittis sapien finibus et. Nulla blandit, mi vel congue lacinia, tur " +
-                "congue purus, vehicula vehicula dolor ligula a odio. Sed auctor eu felis sit amet laoreet. Vivamus scelerisque enim tellus, vel egestas leo digniss" +
-                " vitae urna vitae felis porta pulvinar. Duis vestibulum neque turpis, eget rhoncus magna vestibulum eu. Curabitur sed nunc id nulla faucibus ullamc" +
-                " gravida ornare lacus, in rutrum nisi tempus in. Morbi non nibh eu augue volutpat consectetur eu sed ipsum.&#10;&#10;Donec laoreet arcu ut vehiculs" +
-                " tristique a tortor ac dapibus. Cras metus neque, semper consequat ex quis, pretium luctus tellus. Sed pretium a sem id sollicitudin. Suspendisse a" +
-                " lobortis. Fusce pretium posuere bibendum. Morbi pharetra ante porttitor dignissim imperdiet. Duis nec magna enim. Nulla varius arcu id pellentesqu" +
-                " rutrum ultricies pellentesque. Vestibulum venenatis mauris non pharetra efficitur. Nullam tempus eros viverra accumsan auctor. Morbi mollis, massa" +
-                " pellentesque, magna nunc bibendum sem, vel fringilla lectus elit quis turpis.&#10;&#10;Integer bibendum lorem et libero tempor, a molestie ipsum." +
-                " Integer placerat eros sit amet arcu cursus");
-
+        // Variables
 
         // Card Creation
         HBox card = new HBox();
@@ -86,7 +66,10 @@ public class MovieCardFactoryController implements Initializable {
         gridPane.add(card, column, row);
 
         // Poster
-        Image poster = new Image("https://maxcdn.icons8.com/app/uploads/2019/05/poster-for-movie.png");
+        Image poster = new Image(posterIn);
+        if(poster.isError()) {
+            poster = new Image("file:resources/imgs/noimage.png");
+        }
         ImageView iv = new ImageView();
         iv.setImage(poster);
         iv.setPreserveRatio(false);
@@ -96,22 +79,22 @@ public class MovieCardFactoryController implements Initializable {
 
         // Details side of card
         VBox details = new VBox();
-        Label title = new Label(titleText);
-        Label category = new Label(categoryText);
+        Label title = new Label(titleIn);
+        Label category = new Label(categoryIn);
         ScrollPane detailsPane = new ScrollPane();
         detailsPane.setPrefSize(250,130);
-        Label description = new Label(descriptionText);
+        Label description = new Label(plotIn);
         description.setWrapText(true);
         description.setMaxWidth(230);
         detailsPane.setContent(description);
-        Button button = new Button("Click");
-        button.setMaxWidth(Double.MAX_VALUE);
+//        Button button = new Button("Click");
+//        button.setMaxWidth(Double.MAX_VALUE);
 
         // add to details
         details.getChildren().add(title);
         details.getChildren().add(category);
         details.getChildren().add(detailsPane);
-        details.getChildren().add(button);
+//        details.getChildren().add(button);
 
         // add to card
         card.getChildren().add(iv);
